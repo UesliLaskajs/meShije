96c6399f147cbb7ad7f76ee1b59f180f721c6c78
Uesli Laska <uesli.laska@gmail.com>
2023-10-29 19:07:00 +0100

    Photo change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar/Navbar"
 import { Link } from "react-router-dom"
+// import backgroundImg from "../assets/images/background.jpg"
 const Home = () => {
     return (
         <>
